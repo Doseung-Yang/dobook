5519086621b3757d0d80530706f86a7009fe6c4f
양도승 <doseung.dev@gmail.com>
2026-01-20 18:08:29 +0900

    feat: enhance ESLint configuration and add VSCode settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,17 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unsafe-assignment": "error",
+      "@typescript-eslint/no-unsafe-member-access": "error",
+      "@typescript-eslint/no-unsafe-call": "error",
+      "@typescript-eslint/no-unsafe-return": "error",
+      "@typescript-eslint/no-unsafe-argument": "error",
+      "@typescript-eslint/ban-ts-comment": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   ...(process.env.ANALYZE === 'true' && {
     webpack: (config, { isServer }) => {
       if (!isServer && process.env.ANALYZE === 'true') {
-        // eslint-disable-next-line @typescript-eslint/no-require-imports
         const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
         if (config.plugins) {
           config.plugins.push(
